35a0a2dd99559a96a0389f3ebebed98cbd84ba95
Partho Bala <cspartho19@gmail.com>
2019-10-20 12:58:49 +0600

    updated error functions and dataset for better accuracy

--- Asteroid_collision_avoider_ML.py
+++ Asteroid_collision_avoider_ML.py
@@ -2,7 +2,7 @@
 """
 Created on Fri Oct 18 00:00:26 2019
 
-@author: Linkon
+@author: Linkon from Galactic Angels
 """
 
 import pandas as pd  
@@ -13,7 +13,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
 
-dataset = pd.read_csv(r'Datasets\Asteroid_data.csv')
+dataset = pd.read_csv(r'C:\Users\Hp\Desktop\Nasa\Dataset\Asteroid_data_final.csv')
 
 dataset.shape
 
@@ -22,16 +22,16 @@ dataset.describe()
 dataset.isnull().any()
 dataset = dataset.fillna(method='ffill')
 
-X = dataset[['As_diam_m', 'As_dist_km', 'As_velocity_kmh', 'As_velocity_angle', 'As_dist_relative_flag', 'soron']].values
-y = dataset['Rotate_angle'].values
+X = dataset[['As_diam_km', 'As_dist_km', 'As_velocity_kmh', 'As_velocity_angle', 'As_dist_relative_flag','Coordinate_flag','Sc_diam_m', 'Sc_velocity_kmh', 'Relative_velocity', 'Estimated_time', 'New_theta']].values
+y = dataset['Final_Angle'].values
 
 plt.figure(figsize=(15,10))
 plt.tight_layout()
-seabornInstance.distplot(dataset['Rotate_angle'])
+seabornInstance.distplot(dataset['Final_Angle'])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-regressor = LinearRegression()  
+regressor = LinearRegression()  s
 regressor.fit(X_train, y_train)
 
 #coeff_df = pd.DataFrame(regressor.coef_, X.columns, columns=['Coefficient'])
@@ -49,3 +49,7 @@ df1.plot(kind='bar',figsize=(10,8))
 plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
 plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
 plt.show()
+
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))  
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))  
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
